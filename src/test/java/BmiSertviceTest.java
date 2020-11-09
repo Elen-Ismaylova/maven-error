8867bbcf788ef1fdfcade4227b6387a95a8329a9
Elen Ismaylova <wonderelen@gmail.com>
2020-11-09 17:15:23 +0300

    new commits added

--- src/test/java/BmiSertviceTest.java
+++ src/test/java/BmiSertviceTest.java
@@ -24,7 +24,7 @@ class BmiSertviceTest {
             double bmi = service.calculate(97, 1.65);
             System.out.println(bmi);
 
-            double expected = 30;
+            double expected = 35;
             double actual = 35;
             assertEquals(expected, actual);
 
